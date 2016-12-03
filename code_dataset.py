fabfe9c1f9aa54d180d5bde0cbd5362b1a19433c
LXander <l.xander.233@gmail.com>
2016-12-02 23:28:36 -0500

    bug fix in code_dataset.py

--- code_dataset.py
+++ code_dataset.py
@@ -18,7 +18,7 @@ def dump_answer_list(answer_list,filename):
         for row in answer_list:
             line = row[0] + ','
             line += ' '.join(row[1:])
-            line += '/n'
+            line += '\n'
             fr.write(line)
 
 
@@ -32,24 +32,38 @@ def copy_original(source,dest,raw):
     map(create_folder,folders)
 
     files = raw['path']
+    files = []
     for f in files:
         create_folder(os.path.dirname(os.path.join(dest,f)))
         shutil.copy(os.path.join(source,f),os.path.join(dest,f))
 
 
+    crystal = raw.apply(lambda item:crystal_folder_name+'/'+item['PDBname']+'/'+item['ID']+'_ligand.npy',axis=1)
+
+    crystal = list(set(crystal))
+
+    for rec in crystal:
+        create_folder(os.path.dirname(os.path.join(dest,rec)))
+        shutil.copy(os.path.join(source,rec),os.path.join(dest,rec))
+
+    receptor = raw.apply(lambda item:receptor_folder_name+'/'+item['PDBname']+'.npy',axis=1)
+    receptor = list(set(receptor))
+
+    for rec in receptor:
+        create_folder(os.path.join(dest,'receptor'))
+	shutil.copy(os.path.join(source,rec),os.path.join(dest,rec))
+    
+
+def _copy(item,source = '/n/scratch2/xl198/data/npy',dest='/n/scratch2/xl198/data/test_set'):
+    print os.path.join(source,item['file']) 
+    shutil.copy(os.path.join(source, item['file']),os.path.join(dest,'receptor' if item['code'][0]=='P' else 'ligands',item['code']+".npy"))
+
 def copy_coded(source,dest,lookup):
     append_dest = lambda x: os.path.join(dest, x)
-    folders = map(append_dest, database_folders)
+    folders = map(append_dest, ['receptor','ligands'])
     map(create_folder, folders)
 
-
-    def _copy(item):
-        create_folder(os.path.dirname(os.path.join(dest, item['file'])))
-        shutil.copy(os.path.join(source, item['file']),
-                    os.path.join(os.path.dirname(os.path.join(dest, item['file'])),item['code']+'.pdb'))
-
-
-
+    lookup.apply(_copy,axis=1)
 
 def generate_database_index(database_path):
     crystal_folder = os.path.join(database_path,'crystal')
@@ -68,7 +82,8 @@ def generate_database_index(database_path):
             row.append(os.path.join(ligands_folder_name,filename.split('_')[0],filename))
             # innder_index
             row.append(int(filename.split('.')[0].split('_')[-1]))
-
+            raw.append(row)
+    
     database = pd.DataFrame(data = raw,columns = ['ID','PDBname','path','inner_index'])
 
     dump_dataframe(database, 'database_index')
@@ -83,11 +98,11 @@ def code_test_set(test_set):
     gou = sorted.groupby("ID")
     groups = gou.groups
     raw_answer_list = []
-    for key in groups.key():
+    for key in groups.keys():
         PDBname = key.split('_')[0]
         row = []
-        row.append(os.path.join(receptor_folder_name,PDBname+'.pdb'))
-        row.append(os.path.join(crystal_folder_name,PDBname,key+'.pdb'))
+        row.append(os.path.join(receptor_folder_name,PDBname+'.npy'))
+        row.append(os.path.join(crystal_folder_name,PDBname,key+'_ligand.npy'))
         for value in groups[key]:
             row.append(sorted.ix[value]['path'])
         raw_answer_list.append(row)
@@ -101,7 +116,9 @@ def code_test_set(test_set):
     for raw_row in raw_answer_list:
         coded_row = []
         for i in range(len(raw_row)):
-            coded_row.append('L'+raw_row[i] if i else 'P'+raw_row[i])
+            coded_row.append('L'+str(reindex[cur]) if i else 'P'+str(reindex[cur]))
+            cur +=1
+        coded_answer_list.append(coded_row)
 
     dump_answer_list(raw_answer_list,'raw_solution')
     dump_answer_list(coded_answer_list,'solution')
@@ -109,7 +126,13 @@ def code_test_set(test_set):
     lookup = []
     for i in range(len(coded_answer_list)):
         for j in range(len(coded_answer_list[i])):
-            lookup.append([coded_answer_list[i][j],raw_answer_list[i][j]])
+            if j == 0:
+                lookup.append([coded_answer_list[i][j],'receptor/'+os.path.basename(raw_answer_list[i][j])])
+            elif j == 1:
+		lookup.append([coded_answer_list[i][j],'crystal/'+raw_answer_list[i][j].split('_')[0].split('/')[-1]+'/'+os.path.basename(raw_answer_list[i][j])])
+            else:
+                lookup.append([coded_answer_list[i][j],'ligands/'+raw_answer_list[i][j].split('_')[0].split('/')[-1]+'/'+os.path.basename(raw_answer_list[i][j])])
+
 
     lookup_tabel = pd.DataFrame(data = lookup,columns = ['code','file'])
 

558ad81469122aab771a9214ad0a74244e767346
LXander <l.xander.233@gmail.com>
2016-12-18 17:55:09 -0500

    integrate train_set

--- integrate.py
+++ integrate.py
@@ -4,7 +4,7 @@ from itertools import chain
 import pandas as pd
 
 dest_folder = '/n/scratch2/xl198/data/integrate'
-source_path = '/n/scratch2/xl198/data/kaggle/test_set_npy'
+source_path = '/n/scratch2/xl198/data/kaggle/train_set_npy'
 test_file ='/n/scratch2/xl198/data/integrate/result.npy'
 
 
@@ -19,26 +19,26 @@ def test(i):
     b = np.load(path)
     print np.all(a == b)
 
-def make_index():
+def make_ligand_index():
     if not os.path.exists(dest_folder):
         os.mkdir(dest_folder)
     npy_list = []
     ids = []
     rec = []
     offset = [0]
-    for dirpath,dirnames,filenames in chain(os.walk(os.path.join(source_path,'ligands'))):
+    for dirpath,dirnames,filenames in chain(os.walk(os.path.join(source_path,'crystal_ligands')),os.walk(os.path.join(source_path,'docked_ligands'))):
         for filename in filenames:
             npy = np.reshape(np.load(os.path.join(dirpath,filename)),(-1,4))
-            #ids.append('_'.join(filename.split('.')[0].split('_')[:3]))
-            ids.append(filename.split('.')[0])
+            ids.append('_'.join(filename.split('.')[0].split('_')[:3]))
+            #ids.append(filename.split('.')[0])
             rec.append(dirpath.split('/')[-1])
             offset.append(npy.shape[0])
             npy_list.append(npy)
 
 
-    #one_file = np.vstack(npy_list)
+    one_file = np.vstack(npy_list)
 
-    #np.save(os.path.join(dest_folder,'test_set_ligands.npy'),one_file)
+    np.save(os.path.join(dest_folder,'train_set_ligands.npy'),one_file)
     start = np.array(offset[:-1])
     start = np.cumsum(start)
     end = start + np.array(offset[1:])
@@ -50,7 +50,41 @@ def make_index():
 	data.append([ids[i],rec[i],start[i],end[i]])
 
     record = pd.DataFrame(data=data,columns=['ID','receptor','start','end'])
-    record.to_csv(os.path.join(dest_folder,'test_set_ligands.csv'),index=False)
+    record.to_csv(os.path.join(dest_folder,'train_set_ligands.csv'),index=False)
 
 
-make_index()
+def make_receptor_index():
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
+    npy_list = []
+    ids = []
+    rec = []
+    offset = [0]
+    for dirpath, dirnames, filenames in chain(os.walk(os.path.join(source_path, 'receptors'))):
+        for filename in filenames:
+            npy = np.reshape(np.load(os.path.join(dirpath, filename)), (-1, 4))
+            #ids.append('_'.join(filename.split('.')[0].split('_')[:3]))
+            ids.append(filename.split('.')[0])
+            #rec.append(dirpath.split('/')[-1])
+            offset.append(npy.shape[0])
+            npy_list.append(npy)
+
+    one_file = np.vstack(npy_list)
+
+    np.save(os.path.join(dest_folder,'train_set_receptors.npy'),one_file)
+    start = np.array(offset[:-1])
+    start = np.cumsum(start)
+    end = start + np.array(offset[1:])
+
+    start = list(start)
+    end = list(end)
+    data = []
+    for i in range(len(ids)):
+        data.append([ids[i], start[i], end[i]])
+
+    record = pd.DataFrame(data=data, columns=['ID', 'start', 'end'])
+    record.to_csv(os.path.join(dest_folder, 'train_set_receptors.csv'), index=False)
+
+
+make_ligand_index()
+make_receptor_index()

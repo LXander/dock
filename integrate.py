d6611e9b79b0668f96f77d2d6f6b7bff2c0b5f34
LXander <l.xander.233@gmail.com>
2016-12-18 17:49:32 -0500

    integrate liangds

--- integrate.py
+++ integrate.py
@@ -4,7 +4,7 @@ from itertools import chain
 import pandas as pd
 
 dest_folder = '/n/scratch2/xl198/data/integrate'
-source_path = '/n/scratch2/xl198/data/kaggle_dec_04/test_set_npy'
+source_path = '/n/scratch2/xl198/data/kaggle/test_set_npy'
 test_file ='/n/scratch2/xl198/data/integrate/result.npy'
 
 
@@ -24,19 +24,21 @@ def make_index():
         os.mkdir(dest_folder)
     npy_list = []
     ids = []
+    rec = []
     offset = [0]
-    for dirpath,dirnames,filenames in chain(os.walk(os.path.join(source_path,'receptors'))):
+    for dirpath,dirnames,filenames in chain(os.walk(os.path.join(source_path,'ligands'))):
         for filename in filenames:
             npy = np.reshape(np.load(os.path.join(dirpath,filename)),(-1,4))
             #ids.append('_'.join(filename.split('.')[0].split('_')[:3]))
             ids.append(filename.split('.')[0])
+            rec.append(dirpath.split('/')[-1])
             offset.append(npy.shape[0])
             npy_list.append(npy)
 
 
-    one_file = np.vstack(npy_list)
+    #one_file = np.vstack(npy_list)
 
-    np.save(os.path.join(dest_folder,'result.npy'),one_file)
+    #np.save(os.path.join(dest_folder,'test_set_ligands.npy'),one_file)
     start = np.array(offset[:-1])
     start = np.cumsum(start)
     end = start + np.array(offset[1:])
@@ -45,10 +47,10 @@ def make_index():
     end = list(end)
     data = []
     for i in range(len(ids)):
-	data.append([ids[i],start[i],end[i]])
+	data.append([ids[i],rec[i],start[i],end[i]])
 
-    record = pd.DataFrame(data=data,columns=['ID','start','end'])
-    record.to_csv(os.path.join(dest_folder,'record.csv'),index=False)
+    record = pd.DataFrame(data=data,columns=['ID','receptor','start','end'])
+    record.to_csv(os.path.join(dest_folder,'test_set_ligands.csv'),index=False)
 
 
 make_index()

d17ea15a001785a829324a33852533763f167e82
LXander <l.xander.233@gmail.com>
2017-01-10 22:05:46 -0500

    fix bug in cluster

--- refract/cluster.py
+++ refract/cluster.py
@@ -17,6 +17,9 @@ class cluster:
     def get_file_path(self,sourcePath):
         for dirpath,dirname,filenames in os.walk(sourcePath):
             for filename in filenames:
+                if not len(open(os.path.join(dirpath,filename)).readlines()):
+                    print("empty ",filename)
+                    continue   
                 yield os.path.join(dirpath,filename)
 
     def hierarchy_cluster(self,filePath,clusterNumber = 10):
@@ -24,24 +27,27 @@ class cluster:
         ligand = '_'.join(fileName.split('_')[:3])
         receptor = fileName.split('_')[0]
 
-
+   
 
         traj = md.load(filePath)
+        print("frame number ",traj.n_frames)
         distances = np.empty((traj.n_frames,traj.n_frames))
         for i in range(traj.n_frames):
             distances[i] = md.rmsd(traj,traj,i)
 
         reduced_distances = squareform(distances,checks=False)
 
-        Z = scipy.cluster.hierarchy.linkage(reduced_distances ,methods='average')
+        Z = scipy.cluster.hierarchy.linkage(reduced_distances ,metric='average')
 
-        clusterResule = fcluster(Z,clusterNumber,criterion='maxcluster')
-
-
+        clusterResult = fcluster(Z,clusterNumber,criterion='maxclust').reshape((-1,1))
+        #print(clusterResult.shape)
+        #print(clusterResult)
         ligands = np.array([ligand]*traj.n_frames).reshape((-1,1))
         receptors = np.array([receptor]*traj.n_frames).reshape((-1,1))
-        inner_index = np.arange(1,traj.n_frames+1)
-        labeledResult = np.vstack((ligands,receptors,inner_index,clusterResule)).transpose()
+        inner_index = np.arange(1,traj.n_frames+1).reshape((-1,1))
+        #print(inner_index)
+        #print( "{} {} {} {}".format(ligands.shape[0],receptors.shape[0],inner_index.shape[0],clusterResult.shape[0]))
+        labeledResult = np.hstack((ligands,receptors,inner_index,clusterResult))
 
 
         return labeledResult
@@ -49,9 +55,11 @@ class cluster:
 
 
     def test(self,filePath):
+        print("test")
         filePathes = self.get_file_path(filePath)
-
-        clusterResult = self.hierarchy_cluster(filePathes.next())
+        path = filePathes.next()
+        print(path)    
+        clusterResult = self.hierarchy_cluster(path)
 
         print(clusterResult)
 
@@ -62,6 +70,13 @@ class cluster:
 
         df.to_csv(savePath,index=False)
 
+    def writeDownCluster(self,filePath,savePath):
+        for path in self.get_file_path(filePath):
+            pass        
+
 if __name__ == '__main__':
     c = cluster()
-    c.test("")
+    print("start")
+    #c.test("/n/scratch2/xl198/dude/data/dude/docked_dude/docked_ligands")
+    #c.test("/n/scratch2/xl198/dude/data/dude_400/pdbs/docked_ligands")
+    

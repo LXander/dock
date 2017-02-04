93e5cda6c5219711f9b695c081db5e4f8366e60f
LXander <l.xander.233@gmail.com>
2017-02-04 17:12:34 -0500

    fix mapping

--- dataset/superposition/mapping.py
+++ dataset/superposition/mapping.py
@@ -8,16 +8,18 @@ def mapping():
 
     cry_gou = cry.groupby("atom_num")
     dec_gou = dec.groupby("atom_num")
-
+    dec_gous = dec_gou.groups
     records = []
 
     for name,cry_group in cry_gou:
         print "atom num ",name
-        if not name in dec_group.keys():
+        if not name in dec_gous.keys():
             sys.stderr.write("atom num {} doesn't have corresponding decoys.".format(name))
             continue
         else:
-            dec_group = dec_gou[name]
+            dec_index = dec_gous[name]
+            dec_group = dec.iloc[dec_index]
+            
             dec_len = len(dec_group)
             cry_len = len(cry_group)
             if dec_len<cry_len:
@@ -37,6 +39,9 @@ def mapping():
                 records.append(pair)
 
     df = pd.DataFrame(records,columns=['ligand_box','ligand'])
+    df['ligand_box'] = df['ligand_box'].apply(lambda x:x.strip())
+    df['ligand'] = df['ligand'].apply(lambda x:x.strip())
+    df['receptor'] = df['ligand_box'].apply(lambda x:x.replace("addH",'data').split('_')[0]+'.pdb')
     df.to_csv(os.path.join(FLAGS.formsFolder,'docking_pair.csv'),index=False)
 
 

3f2174b833507f691660484a54a73330f0ecbc45
LXander <l.xander.233@gmail.com>
2017-02-07 20:09:33 -0500

    add mean calculate

--- dude/smina_auc.py
+++ dude/smina_auc.py
@@ -2,6 +2,47 @@ import os,sys
 import pandas as pd
 import numpy as np
 
+def calculate_mean(filePath,k):
+    '''
+    columns in the file filename,inner_id,energy
+    :param filePath:
+    :param k:
+    :return:
+    '''
+
+    print "convert mean ",filePath
+    filename = os.path.basename(filePath)
+    score_file = pd.read_csv(filePath)
+    cutoff_position = score_file[score_file['inner_id'] <= k]
+
+    gou = cutoff_position.groupby('filename')
+    records = []
+    for name, group in gou:
+        records.append([name,np.mean(group['energy'])])
+
+    reduced_score = pd.DataFrame(data = records,columns = ['filename','energy'])
+    reduced_score['label'] = reduced_score['filename'].apply(lambda x:1 if x.find('actives')>=0 else 0)
+    sorted_score = reduced_score.sort('energy')
+    labels = sorted_score['label']
+
+
+    points = []
+    total = len(labels)
+    ones = sum(labels)
+    zeros = total - ones
+    for i in range(total):
+        points.append([1.0 * sum(labels[:i]) / ones, 1.0 * (i - sum(labels[:i])) / zeros])
+
+    np_points = np.array(points)
+    auc = np.trapz(np_points[:, 0], x=np_points[:, 1])
+
+    with open(FLAGS.auc_log_file, 'a') as fout:
+        fout.write(filename.split('.')[0] + '\t' + str(auc) + '\n')
+
+    df = pd.DataFrame(data=np_points, columns=['tpr', 'fpr'])
+    df.to_csv(os.path.join(FLAGS.destPath, filename.replace('.', 'mean_{}_curve.'.format(k))))
+
+
 def calculate(filePath):
     '''
     Given csv store smina score, calculate the auc based on the best position's smina score
@@ -46,8 +87,8 @@ def run(index=None):
 
 class FLAGS:
     sourcePath = '/n/scratch2/xl198/dude/frame/affinity'
-    auc_log_file = '/n/scratch2/xl198/dude/frame/curve/auc.txt'
     destPath = '/n/scratch2/xl198/dude/frame/curve'
+    auc_log_file = os.path.join(destPath,'mean_auc.txt')
 
 if __name__ == '__main__':
     args = sys.argv

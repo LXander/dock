6d0c4aa6216f9a7afb089b62a64fe2bcecf1f9b8
LXander <l.xander.233@gmail.com>
2017-02-08 16:57:05 -0500

    select spedific frame from DUD-E docking result

--- dude/frame_select.py
+++ dude/frame_select.py
@@ -1,10 +1,25 @@
 import os,sys
 import numpy as np
 from glob import glob
+sys.path.append(os.path.dirname(sys.path[0]))
+from util.createfolder import try_create_chain_parent_folder
 
 def select():
-    receptor_list = glob(os.path.join())
+    ligand_list = glob(os.path.join(FLAGS.sourcePath,'*','*.pdb'))
+    for ligand in ligand_list:
+        dest_ligand = ligand.replace(FLAGS.sourcePath,FLAGS.destPath)
+        dest_ligand = dest_ligand.replace('.','_1.')
+        try_create_chain_parent_folder(dest_ligand)
+        cmd = 'obabel -ipdb {} -f 1 -l 1 -opdb -O {}'.format(ligand,dest_ligand)
+        print cmd
+
+
+
 
 class FLAGS:
-    sourcePath = ''
-    destPath = ''
+    sourcePath = '/n/scratch2/xl198/dude/data/dude/pdbs/ligands'
+    destPath = '/n/scratch2/xl198/dude/data/dude_1'
+
+
+if __name__ == '__main__':
+    select()

28d55e5de07c08c02800bf4c0bf60e40eca182d9
LXander <l.xander.233@gmail.com>
2017-02-10 21:21:34 -0500

    donwload pdb from rcsb

---
+++ dataset/superposition/filter.py
@@ -0,0 +1,285 @@
+import os,sys
+import pandas as pd
+import numpy as np
+import tempfile
+import re
+from glob import glob
+import mdtraj as md
+import prody
+import getopt
+from av4_atomdict import atom_dictionary
+sys.path.append(os.path.dirname(os.path.dirname(sys.path[0])))
+from util.createfolder import try_create_chain_parent_folder
+
+
+
+def get_similar_crystal_file(crystalFolder,ligandPath):
+    '''
+    compare the ligand with all the crystal ligand for the target receptor
+    return the path of the crystal ligand which have a high similarity
+
+    :param crystalFolder: folder store crystal ligands
+    :param ligandPath: path for docking result
+    :return: list of path
+    '''
+
+    file_name= os.path.basename(ligandPath)
+    temp_file_path = os.path.join(FLAGS.tempPath,file_name)
+    print 'obabel convert ',file_name
+    obabel_cmd = 'obabel -ipdb {} -l 1 -f 1 -opdb -O {}'.format(ligandPath,temp_file_path)
+    os.system(obabel_cmd)
+
+
+    crystalList = glob(os.path.join(crystalFolder,'*.pdb'))
+
+    records = []
+
+    for crystal_ligand in crystalList:
+        command = os.popen('babel -d {} {} -ofpt -xfFP4'.format(crystal_ligand,temp_file_path))
+        ls = command.read()
+        try:
+            tanimoto_similarity = re.split('=|\n', ls)[2]
+            if tanimoto_similarity>FLAGS.tanimoto_cutoff:
+                records.append(crystal_ligand)
+        except:
+            records.append(crystal_ligand)
+
+
+
+    return records
+
+def parsePDB(file_path):
+    '''
+    given path of the docking result
+    :param file_path:
+    :return:
+    '''
+
+    lig = prody.parsePDB(file_path)
+    coords = lig.getCoordsets()
+    atom_type = lig.getElements()
+
+    file_name = os.path.basename(file_path).split('.')[0]
+    file_id = re.search('([a-zA-Z0-9]{4}_\d+)',file_name).group()
+    file_dataset = re.search('ligand_(.+)$', file_name).groups()[0]
+    if file_id == None or file_dataset == None:
+        message = "cannot parse Id or dataset for {}".format(file_path)
+        raise(message)
+
+
+    return coords,atom_type
+
+def parseLigand(fast_file_path):
+    '''
+    get the ligand's coordinate of whole protein fast docking, whole rigorous docking
+    and site only rigorous docking
+    :param fast_file_path:
+    :return: coords,atom_type,affinity
+    '''
+    fast_name = os.path.basename(fast_file_path)
+    receptor = fast_name.split('_')[0]
+    original_name = fast_name.replace('ligand_fast','ligand')
+    rigor_name= fast_name.replace('ligand_fast','ligand_rigor')
+    rigor_so_name = fast_name.replace('ligand_fast','ligand_rigor_so')
+
+    rigor_file_path = os.path.join(FLAGS.rigor_path,receptor,rigor_name)
+    rigor_so_file_path= os.path.join(FLAGS.rigor_so_path,receptor,rigor_so_name)
+
+    coords,atom_type,affinity= parsePDB(fast_file_path)
+
+    if os.path.exists(rigor_file_path):
+        rigor_coords,rigor_atom_type,rigor_affinity = parsePDB(rigor_file_path)
+        if rigor_coords == None:
+            # if failed to parse PDB just ignore it
+            pass
+        elif rigor_atom_type == atom_type:
+            coords =np.concatenate((coords,rigor_coords))
+            affinity = np.concatenate((affinity,rigor_affinity))
+        else:
+            message = 'different atom type for {} and {}\n'.format(fast_name,rigor_name)
+            sys.stderr.write(message)
+            raise Exception(message)
+
+    if os.path.exists(rigor_so_file_path):
+        rigor_so_coords,rigor_so_atom_type,rigor_so_affinity = parsePDB(rigor_so_file_path)
+        if rigor_so_coords == None:
+            # if failed to parse PDB just ignore it
+            pass
+        elif rigor_so_atom_type == atom_type:
+            coords = np.concatenate((coords,rigor_so_coords))
+            affinity = np.concatenate((affinity,rigor_so_affinity))
+        else:
+            message = 'different atom type for {} and {}\n'.format(fast_name,rigor_so_name)
+            sys.stderr.write(message)
+            raise Exception(message)
+
+    # coord read by mdtraj is 10 times smaller than
+    # literally record in the pdb file
+    coords = coords*10
+    return coords,atom_type,affinity
+
+def overlap_filter(crystal_list, ligand_coords, ligand_affinity):
+
+
+    for crystal_ligand_path in crystal_list:
+        crystal_ligand = md.load(crystal_ligand_path)
+        # the shape of crystal coords is [1,n,3]
+        # coordinate read by mdtraj is 10 time less than
+        # literally so we times it by 10
+        crysta_coords = crystal_ligand.xyz*10
+
+        # [x,y,1,1,3]
+        exp_ligand_coord = np.expand_dims(np.expand_dims(ligand_coords, -2), -2)
+        print "exp_ligand_coord ",exp_ligand_coord.shape
+        # [x,y,1,n,3]
+        diff = exp_ligand_coord - crysta_coords
+        print "diff ",diff.shape
+        # [x,y,n]
+        distance = np.squeeze(np.sqrt(np.sum(np.square(diff),-1)))
+        print "distance ",distance.shape
+        # [x,y]
+        atom_overlap =(np.sum((distance<FLAGS.clash_cutoff_A).astype(np.float32),-1)>0).astype(np.float32)
+        print "atom_overlap ",atom_overlap.shape
+        # [x]
+        ligand_not_overlap = np.mean(np.squeeze(atom_overlap),-1)<FLAGS.clash_size_cutoff
+        print "ligand_not_overlap num",np.sum(ligand_not_overlap)
+        print crystal_ligand_path
+        ligand_coords = ligand_coords[ligand_not_overlap]
+        ligand_affinity = ligand_affinity[ligand_not_overlap]
+
+    sorted_index = np.argsort(ligand_affinity)
+    ligand_coords = ligand_coords[sorted_index]
+    ligand_affinity = ligand_affinity[sorted_index]
+
+    return ligand_coords, ligand_affinity
+
+def save_av4(filepath,labels,elements,multiframe_coords):
+    concatenated_coords = multiframe_coords[0]
+    for coords in multiframe_coords[1:]:
+        concatenated_coords = np.hstack((concatenated_coords,coords))
+
+    labels = np.asarray(labels*100,dtype=np.int32)
+    elements = np.asarray(elements,dtype=np.int32)
+    concatenated_coords = np.asarray(concatenated_coords,dtype=np.float32)
+
+    if not(int(len(concatenated_coords[:,0])==int(len(elements)))):
+        raise Exception('Number of atom elements is not equal, elements num {}, coords size {}'.format(len(elements),len(concatenated_coords[:,0])))
+
+    if not(int(len(concatenated_coords[0,:])==int(3*len(labels)))):
+        raise Exception('Number labels is not equal to the number of coordinate frames')
+
+    number_of_examples = np.array([len(labels)],dtype=np.int32)
+    av4_record = number_of_examples.tobytes()
+    av4_record += labels.tobytes()
+    av4_record += elements.tobytes()
+    av4_record += multiframe_coords.tobytes()
+    f = open(filepath,'w')
+    f.write(av4_record)
+    f.close()
+
+def convert(fast_file_path):
+
+    fast_name = os.path.basename(fast_file_path)
+    mix_name = fast_name.replace('ligand_fast','ligand_mix')
+    mix_name = mix_name.replace('.pdb','.av4')
+    receptor = fast_name.split('_')[0]
+
+    crystalFolder = os.path.join(FLAGS.crystalPath, receptor)
+    similar_crystal = get_similar_crystal_file(crystalFolder,fast_file_path)
+
+    ligand_coords,ligand_atom_type,ligand_afinity = parseLigand(fast_file_path)
+
+    print 'parsed {} , shape'.format(fast_name)
+    print ligand_coords.shape
+
+    filtered_coords,filtered_affinity = overlap_filter(similar_crystal,ligand_coords,ligand_afinity)
+
+
+    mix_dest_path = os.path.join(FLAGS.mix_path,receptor,mix_name)
+
+    try_create_chain_parent_folder(mix_dest_path)
+
+    save_av4(mix_dest_path,filtered_affinity,ligand_atom_type,filtered_coords)
+
+def convert(fast_path):
+    file_name = os.path.basename(fast_path)
+    file_id = re.search('(^[a-zA-Z0-9]{4}_\d+)', file_name).group()
+    receptor = file_name.split('_')[0]
+
+    crystalFolder = os.path.join(FLAGS.crystalPath, receptor)
+
+    similar_crystal = get_similar_crystal_file(crystalFolder, fast_path)
+    lig = prody.parsePDB(fast_path)
+    lig_coords = lig.getCoordsets()
+    lig_elements = lig.getElements()
+
+
+
+
+
+
+
+
+def run():
+    FLAGS.tempPath = tempfile.mkdtemp()
+
+    fastFileList = glob(os.path.join(FLAGS.super_source, '*', '*.pdb'))
+
+    index = range(len(fastFileList))
+
+    if hasattr(FLAGS,'arrayjob') and FLAGS.arrayjob:
+        if hasattr(FLAGS,'offset'):
+            index = [len(index)/FLAGS.jobsize*FLAGS.offset+FLAGS.jobid]
+        else:
+            index = range(FLAGS.jobid-1,len(index),FLAGS.jobsize)
+
+    for i in index:
+        convert(fastFileList[i])
+
+class FLAGS:
+    super_source = '/n/scratch2/xl198/data/Superposition/dock'
+    fast_path = '/n/scratch2/xl198/data/pdbs'
+    rigor_path = '/n/scratch2/xl198/YI/rigor/final'
+    rigor_so_path = '/n/scratch2/xl198/YI/rigor_so/final'
+    mix_path = '/n/scratch2/xl198/data/fusion/dock'
+    crystalPath = '/n/scratch2/xl198/data/H/addH'
+    dataframe =pd.read_csv("/n/scratch2/xl198/data/fusion/forms/simple_mix.csv")
+
+    tanimoto_cutoff = 0.75
+    clash_cutoff_A = 4
+    clash_size_cutoff = 0.3
+
+def parse_FLAG():
+    try:
+        opts,args = getopt.getopt(sys.argv[1:],None,["offset=","jobsize=","jobid=","cores=","scan"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+
+        sys.exit(2)
+
+    for name,value in opts:
+        if name == '--offset':
+            FLAGS.joboffset = int(value)
+        if name == '--jobsize':
+            FLAGS.jobsize = int(value)
+            print "--jobsize ",value
+        if name == '--jobid':
+            FLAGS.jobid = int(value)
+            print "--jobid", value
+        if name == '--cores':
+            FLAGS.cores = int(value)
+        if name == '--scan':
+            FLAGS.scan = True
+
+    if hasattr(FLAGS,"jobsize") and hasattr(FLAGS,"jobid"):
+        FLAGS.arrayjob = True
+
+    print "orchestra job ",FLAGS.arrayjob
+
+    if hasattr(FLAGS,'cores'):
+        print "cores num ",FLAGS.cores
+
+if __name__ == '__main__':
+    parse_FLAG()
+    run()
---
+++ downloads/downloadPDB.py
@@ -0,0 +1,256 @@
+import os,sys
+import prody
+import re
+import getopt
+import pandas as pd
+import numpy  as np
+import multiprocessing
+import threading
+sys.path.append(os.path.dirname(sys.path[0]))
+from util.createfolder import try_create_chain_parent_folder,try_create_chain_folder
+
+
+class parseRCSB:
+
+    def __init__(self, folerName):
+        '''
+        Create folder for kaggle dataset
+        tempFolderPath used to store intermediate csv file
+        kaggleBasePath used to store converted and coded data
+        :param folerName:
+        '''
+        test = 0
+
+
+    def write_dataframe(self, dataframe, filename):
+        '''
+        Write dataframe to tempFolderPath
+        :param dataframe: pandas DataFrame
+        :param filename:  name of output file without suffix
+        :return:
+        '''
+        dataframe.to_csv(os.path.join(self.tempFolderPath, filename + '.csv'), index=False)
+
+
+    def error_log(self,content):
+        with open(FLAGS.log_file,'a') as fout:
+            fout.write(content)
+
+
+    def downloads(self,item):
+
+        os.system('cd {}'.format(FLAGS.rowdata_folder))
+        address = FLAGS.address(item)
+        os.system('wget {}'.format(address))
+
+        pdbname = item.lower()
+        ligand_folder = os.path.join(FLAGS.splited_ligand_folder,pdbname)
+        try_create_chain_folder(ligand_folder)
+
+        try:
+            parsed = prody.parsePDB(os.path.join(FLAGS.rowdata_folder,item+'.pdb'))
+        except:
+            self.error_log('can not parse {}.\n'.format(item))
+            return None
+
+        hetero = parsed.select('(hetero and not water) or resname ATP or resname ADP')
+        receptor = parsed.select('protein or nucleic')
+
+        ligand_flags = False
+        for each in prody.HierView(hetero).iterResidues():
+            if each.numAtoms() <= 10:
+                continue
+            else:
+                ligand_flags = True
+                ResId = each.getResindex()
+                ligand_path = os.path.join(FLAGS.splited_ligand_folder,pdbname,"{}_{}_ligand.pdb".format(pdbname,ResId))
+                try_create_chain_parent_folder(ligand_path)
+                prody.writePDB(ligand_path,each)
+
+        if ligand_flags:
+            receptor_path = os.path.join(FLAGS.splited_receptor_folder,pdbname+'.pdb')
+            prody.writePDB(receptor_path,receptor)
+        else:
+            self.error_log("{} doesn't convert, not ligand have more than 10 atoms.\n")
+
+
+
+    def entry_convert(self, item, coded):
+        '''
+        original pdb file contains multiple frames
+        use obabel for select one frame from it
+
+        :param item: row of dataframe
+        :param coded: bool
+        :return:
+        '''
+        source_file_path = item["SourcePath"]
+        if coded:
+            dest_file_path = item['code_destpath']
+        else:
+            dest_file_path = item['DestPath']
+        Id = item['FrameId']
+
+        cmd = 'obabel -ipdb {} -f {} -l {} -opdb -O {}'.format(source_file_path, Id, Id, dest_file_path)
+
+        try:
+            create_chain_parent_folder(dest_file_path)
+        except:
+            pass
+        if not os.path.exists(dest_file_path):
+            os.popen(cmd)
+
+    def thread_convert(self, func, dataframe, coded, index):
+        '''
+        job running in thread
+        :param func: function running in single thread
+        :param dataframe: pandas.DataFrame
+        :param coded: bool
+        :param index: list of int
+        :return:
+        '''
+        for i in index:
+            # print "dataframe size: {}, ix {}".format(len(dataframe),i)
+            func(dataframe[i], coded)
+
+    def process_convert(self, func, dataframe, index):
+
+        # linspace contain end value but range don't
+        # so we use edge[i+1] to select value in index
+        # end should be len(index)-1
+
+
+        if len(index) < self.thread_num:
+            for i in index:
+                func(dataframe.iloc[i])
+            return
+
+        edge = np.linspace(0, len(index) - 1, self.thread_num + 1).astype(int)
+
+        thread_list = [threading.Thread(target=self.thread_convert,
+                                        args=(func,
+                                              dataframe,
+                                              range(index[edge[i]],
+                                                    index[edge[i + 1]])))
+                       for i in range(self.thread_num)]
+
+        for t in thread_list:
+            # print "thread start: ",t
+            t.start()
+
+        for t in thread_list:
+            t.join()
+
+    def convert(self):
+        '''
+        according the result of 'database_from_csv'
+        running multiprocess to get result
+
+        :param dataframe: pandas.DataFrame
+                          string
+        :param coded: bool
+        :return:
+        '''
+
+        convert_func = self.downloads
+
+
+
+        if hasattr(FLAGS, 'cores'):
+            self.process_num = FLAGS.cores
+
+
+
+        if FLAGS.orchestra_arrayjob:
+            jobsize = FLAGS.orchestra_jobsize
+            jobid = FLAGS.orchestra_jobid
+
+            # using iloc to slice dataframe result doesn't contains end [start,end)
+            linspace = np.linspace(0, len(FLAGS.pdb_list), jobsize + 1).astype(int)
+            dataframe = FLAGS.pdb_list[linspace[jobid - 1]:linspace[jobid]]
+
+        print "dataframe size ", len(dataframe)
+
+        # when there's not enough entry to comvert , decrease thread's num
+        if len(dataframe) < self.process_num * self.thread_num:
+
+            for i in range(len(dataframe)):
+                convert_func(dataframe[i])
+            return
+        edge = np.linspace(0, len(dataframe), self.process_num + 1).astype(int)
+        process_list = [multiprocessing.Process(target=self.process_convert,
+                                                args=(convert_func,
+                                                      dataframe,
+                                                      range(edge[i],
+                                                            edge[i + 1])))
+                        for i in range(self.process_num)]
+
+        for p in process_list:
+            print "process start: ", p
+            p.start()
+
+        for p in process_list:
+            print "process end: ", p
+            p.join()
+
+
+
+
+class FLAGS:
+
+    workplace = '/n/scratch2/xl198/data/rcsb'
+    rowdata_folder = os.path.join(workplace,'row')
+    splited_receptor_folder = os.path.join(workplace,'row_receptor')
+    splited_ligand_folder = os.path.join(workplace,'ligands')
+    address = lambda PDB:'https://files.rcsb.org/download/'+PDB+'.pdb'
+    log_file = 'error.log'
+    thread_num = 16
+    process_num = 12
+
+
+
+
+
+
+def parse_FLAG():
+    try:
+        opts,args = getopt.getopt(sys.argv[1:],None,["offset=","jobsize=","jobid=","cores=","scan"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+
+        sys.exit(2)
+
+    for name,value in opts:
+        if name == '--offset':
+            FLAGS.joboffset = int(value)
+        if name == '--jobsize':
+            FLAGS.jobsize = int(value)
+            print "--jobsize ",value
+        if name == '--jobid':
+            FLAGS.jobid = int(value)
+            print "--jobid", value
+        if name == '--cores':
+            FLAGS.cores = int(value)
+        if name == '--scan':
+            FLAGS.scan = True
+
+    if hasattr(FLAGS,"jobsize") and hasattr(FLAGS,"jobid"):
+        FLAGS.arrayjob = True
+
+    print "orchestra job ",FLAGS.arrayjob
+
+    if hasattr(FLAGS,'cores'):
+        print "cores num ",FLAGS.cores
+
+
+    content = open('target_PDB.txt').readline()
+    content = content.split(',')
+    content = map(lambda x:x.strip(),content)
+    FLAGS.pdb_list= content
+
+
+if __name__ == '__main__':
+    parse_FLAG()
+    parser = parseRCSB()
+    parser.convert()

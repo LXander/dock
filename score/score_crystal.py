8e8f36cccd4b4fa6be3c4602ab2c2f5cb7a3c201
LXander <l.xander.233@gmail.com>
2017-02-14 13:50:58 -0500

    refract score

--- score/score_crystal.py
+++ score/score_crystal.py
@@ -1,24 +1,73 @@
 import os,sys
-import pandas
+import re
+import pandas as pd
 from glob import glob
+import getopt
+sys.path.append(os.path.dirname(sys.path[0]))
+from util.orchestra import orchestra_job
+from util.createfolder import try_create_chain_parent_folder
 
 smina = '/home/xl198/program/smina/smina.static'
 
+class Score(orchestra_job):
+    arrayjob = False
+    workplace = '/n/scratch2/xl198/data/score'
+    formsPath = os.path.join(workplace,'forms')
+    dockSourcePath = '/n/scratch2/xl198/dude/data/dude/pdbs/ligands'
+    smina = '/home/xl198/program/smina/smina.static'
+    thread_num = 1
+    process_num = 1
 
 
-def scoring(crystal_path):
-    file_name = os.path.basename(crystal_path)
-    receptor = file_name.split('_')[0]
-    receptor_path = os.path.dirname(crystal_path).replace('addH','data')
-    receptor_file_path = os.path.join(receptor_path,receptor+'.pdb')
-    cmd = '{} -r {} -l {} --score_only'
-    result = os.popen(cmd)
-    print cmd
+    def __init__(self):
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], None, ["jobsize=", "jobid=", "cores="])
+        except getopt.GetoptError as err:
+            # print help information and exit:
+            print str(err)  # will print something like "option -a not recognized"
 
-def get_score():
-    crystal_list = glob(os.path.join('/n/scratch2/xl198/data/H/addH','*','*.pdb'))
-    for crystal in crystal_list:
-        scoring(crystal)
+            sys.exit(2)
+
+        for name, value in opts:
+            if name == '--jobsize':
+                self.jobsize = int(value)
+                print "--jobsize ", value
+            if name == '--jobid':
+                self.jobid = int(value)
+                print "--jobid", value
+            if name == '--cores':
+                self.cores = int(value)
+
+        if hasattr(self, "jobsize") and hasattr(self, "jobid"):
+            self.arrayjob = True
+
+        print "orchestra job ", self.arrayjob
+
+        if hasattr(self, 'cores'):
+            print "cores num ", self.cores
+
+        try_create_chain_parent_folder(self.workplace)
+        try_create_chain_parent_folder(self.formsPath)
+
+    def scoring(self,crystal_path):
+        file_name = os.path.basename(crystal_path)
+        ligand_id = '_'.join(file_name.split('_')[:2])
+        receptor = file_name.split('_')[0]
+        receptor_path = os.path.dirname(crystal_path).replace('addH','data')
+        receptor_file_path = os.path.join(receptor_path,receptor+'.pdb')
+
+        cmd = '{} -r {} -l {} --score_only'.format(self.smina,receptor_file_path,crystal_path)
+        print cmd
+        result = os.popen(cmd)
+        smina_score = re.search('Affinity: (-?\d+.\d+) \(kcal/mol\)',result).groups()[0]
+
+        with open(os.path.join(self.formsPath,'score_'+str(self.jobid)+'.txt'),'a') as fout:
+            fout.write(ligand_id + ',' + smina_score+'\n')
+
+    def get_score(self):
+        crystal_list = glob(os.path.join('/n/scratch2/xl198/data/H/addH', '*', '*.pdb'))
+        self.convert(crystal_list,self.scoring)
 
 if __name__ == '__main__':
-    get_score()
+    score = Score()
+    score.get_score()
---
+++ util/orchestra.py
@@ -0,0 +1,115 @@
+import os,sys
+import re
+import numpy as np
+import pandas as pd
+import getopt
+
+import threading
+import multiprocessing
+
+from createfolder import try_create_chain_folder,try_create_chain_parent_folder
+
+
+class orchestra_job:
+
+
+    def get(self,dataframe,index):
+        '''
+
+        :param dataframe: pandas.DataFrame or list
+        :param index:
+        :return: return index-th value from dataframe
+        '''
+        if isinstance(dataframe,pd.DataFrame):
+            return dataframe.iloc[index]
+        elif isinstance(dataframe,list):
+            return dataframe[index]
+        else:
+            message = "doesn't support dataframe type ",type(dataframe)
+            raise Exception(message)
+
+    def thread_job(self,func,dataframe,index):
+        '''
+        run func for each item in dataframe
+
+        :param func:
+        :param dataframe: pandas.DataFrame or list
+        :param index: list[int]
+        :return:
+        '''
+        for i in index:
+            func(self.get(dataframe,i))
+
+    def process_job(self,func,dataframe,index):
+        '''
+
+        :param func:
+        :param dataframe: pandas.DataFrame or list
+        :param index: list[int]
+        :return:
+        '''
+
+        if len(index) < self.thread_num:
+            # if job num not enough, running job in process
+            for i in index:
+                func(self.get(dataframe,i))
+        else:
+            # create thread to run job
+            edge = np.linspace(0, len(index) - 1, self.thread_num + 1).astype(int)
+
+            thread_list = [threading.Thread(target=self.thread_job,
+                                            args=(func,
+                                                  dataframe,
+                                                  range(index[edge[i]],
+                                                        index[edge[i + 1]])))
+                           for i in range(self.thread_num)]
+
+            for t in thread_list:
+                # print "thread start: ",t
+                t.start()
+
+            for t in thread_list:
+                # print "thread end:",t
+                t.join()
+
+    def convert(self,dataframe,convert_func):
+        '''
+
+        :param dataframe:
+        :param convert_func:
+        :return:
+        '''
+
+        if hasattr(self, 'cores'):
+            self.process_num = self.cores
+
+        # if get a str, read csv
+        if type(dataframe) == str:
+            try:
+                dataframe = pd.read_csv(os.path.join(self.formsFolder, dataframe))
+            except Exception as e:
+                print e
+
+        if self.arrayjob:
+            linspace = np.linspace(0, len(dataframe), self.jobsize + 1).astype(int)
+            dataframe = dataframe.iloc[linspace[self.jobid - 1]:linspace[self.jobid]]
+
+        if len(dataframe) < self.process_num * self.thread_num:
+            for i in range(len(dataframe)):
+                convert_func(self.get(dataframe,i))
+        else:
+            edge = np.linspace(0, len(dataframe), self.process_num + 1).astype(int)
+            process_list = [multiprocessing.Process(target=self.process_job,
+                                                    args=(convert_func,
+                                                          dataframe,
+                                                          range(edge[i],
+                                                                edge[i + 1])))
+                            for i in range(self.process_num)]
+
+        for p in process_list:
+            print "process start: ", p
+            p.start()
+
+        for p in process_list:
+            print "process end: ", p
+            p.join()
